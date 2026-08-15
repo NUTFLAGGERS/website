5f2b5fd2efcbcee8bd90c8e5da9e7bb1e2002fd3
maejikal <meylinggga@gmail.com>
2026-08-15 11:56:30 +0800

    updated base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+const isProd = process.env.NODE_ENV === 'production' || process.argv.includes('build');
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: 'https://nutflaggers.github.io',
-  base: '/website/',
+  base: isProd ? '/website/' : '/',
   markdown: {
     shikiConfig: {
       theme: 'github-dark-dimmed',
